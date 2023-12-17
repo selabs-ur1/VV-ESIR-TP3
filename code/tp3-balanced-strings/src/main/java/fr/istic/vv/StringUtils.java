0a64e80adf0fae477fce5853bfafdb692e4e612d
Emeraudia <stevan_metayer@orange.fr>
2023-12-17 15:21:57 +0100

    Remove .class & ex3

--- code/tp3-balanced-strings/src/main/java/fr/istic/vv/StringUtils.java
+++ code/tp3-balanced-strings/src/main/java/fr/istic/vv/StringUtils.java
@@ -1,11 +1,30 @@
 package fr.istic.vv;
 
+import java.util.Stack;
+
 public class StringUtils {
 
-    private StringUtils() {}
-
-    public static boolean isBalanced(String str) {
-        return false;
+    private StringUtils() {
     }
 
+    public static boolean isBalanced(String str) {
+        Stack<Character> symbols = new Stack<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+
+            if (c == '(' || c == '[' || c == '{') {
+                symbols.add(c);
+            } else {
+                if (c == ')' && (symbols.empty() || '(' != symbols.pop())) {
+                    return false;
+                } else if (c == ']' && (symbols.empty() || '[' != symbols.pop())) {
+                    return false;
+                } else if (c == '}' && (symbols.empty() || '{' != symbols.pop())) {
+                    return false;
+                }
+            }
+        }
+        return symbols.empty();
+    }
 }
